a0c98e55619a76dd446849f8ff13ba2cd0f1e5fb
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-04-27 14:15:38 +0530

    Home and nav component updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Nav from "@/components/Home/Nav";
 
 const font = Plus_Jakarta_Sans({ weight: ['200', '300', '400', '500', '600', '700', '800'], subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
