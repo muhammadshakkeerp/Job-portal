147d37d3c90fbfbb6ae6b3b381c08dfcc5b16acf
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-04-30 15:18:59 +0530

    social icons added

--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa6'
 
 const Footer = () => {
     return (
@@ -9,6 +10,20 @@ const Footer = () => {
                     <h1 className='text-2xl text-white mb-4 font-bold uppercase'>Jobify</h1>
                     <p className='text-sm text-white text-opacity-70'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit tempore obcaecati excepturi, voluptatem, odio deserunt.</p>
                     {/* social icons */}
+                    <div className='mt-6 flex items-center space-x-3'>
+                        <div className='w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center flex-col'>
+                            <FaFacebook className='text-white'/>
+                        </div>
+                        <div className='w-10 h-10 bg-sky-600 rounded-full flex items-center justify-center flex-col'>
+                            <FaTwitter className='text-white'/>
+                        </div>
+                        <div className='w-10 h-10 bg-red-600 rounded-full flex items-center justify-center flex-col'>
+                            <FaYoutube className='text-white'/>
+                        </div>
+                        <div className='w-10 h-10 bg-pink-600 rounded-full flex items-center justify-center flex-col'>
+                            <FaInstagram className='text-white'/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
