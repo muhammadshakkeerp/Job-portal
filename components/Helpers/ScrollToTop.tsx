81a63c7b096e8f9df1b9e00d1a901d1946817780
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-05-01 19:29:10 +0530

    finished scrollToTop

--- components/Helpers/ScrollToTop.tsx
+++ components/Helpers/ScrollToTop.tsx
@@ -1,4 +1,6 @@
+"use client"
 import React, { useEffect, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa6'
 
 export const ScrollToTop = () => {
     const [isVisible, setIsVisible] = useState(false)
@@ -15,7 +17,19 @@ export const ScrollToTop = () => {
             window.removeEventListener("scroll", toggleVisibility)
         }
     }, [])
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
     return (
-        <div>ScrollToTop</div>
+        <div className='fixed bottom-4 right-4'>
+            {isVisible && <button onClick={scrollToTop} title='Press to go top' type='button' className='bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center'>
+                <FaArrowUp />
+            </button>}
+        </div>
     )
 }
