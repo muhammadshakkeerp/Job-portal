5cd83f32c26b61a6dd5435d5990fa3bb3ac258b4
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-05-02 13:08:23 +0530

    finding clicked job details

--- app/job/jobDetails/[id]/page.tsx
+++ app/job/jobDetails/[id]/page.tsx
@@ -1,8 +1,13 @@
+import JobData, { Job } from '@/data'
 import React from 'react'
 
-const JobDeatils = () => {
+const JobDeatils = async ({ params }: { params: { id: string } }) => {
+  const getJobDetail = JobData?.find((job) => job.id.toString() === params.id)
+  console.log(getJobDetail)
   return (
-    <div>Jobdeatils</div>
+    <div>
+
+    </div>
   )
 }
 
