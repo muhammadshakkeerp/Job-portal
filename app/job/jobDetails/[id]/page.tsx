7164b806e3db106baef801451ded11882eb11eee
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-05-02 15:02:26 +0530

    Retrieve and display job details data

--- app/job/jobDetails/[id]/page.tsx
+++ app/job/jobDetails/[id]/page.tsx
@@ -1,8 +1,17 @@
+import JobData from '@/data'
 import React from 'react'
 
-const JobDeatils = () => {
+const JobDeatils = ({ params }: { params: { id: string } }) => {
+  const getJobDetail = JobData?.find((job) => job.id.toString() === params.id)
+  console.log(getJobDetail)
   return (
-    <div>Jobdeatils</div>
+    <div>
+
+      <div>Jobdeatils</div>
+      <div>
+        {JSON.stringify(getJobDetail)}
+      </div>
+    </div>
   )
 }
 
